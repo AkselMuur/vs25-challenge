4db96e2ed191323993674cdf19584e1d07ea5ffc
Kait-e <Kait-evander.uusimaa@voco.ee>
2026-04-02 11:56:38 +0300

    YL3 - Meals in console

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,23 @@
+
 const Meals = () => {
+
+   fetchData();
+   async function fetchData() {
+    try{
+        const response = await fetch("http://localhost:3001/meals")
+        
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    }
+    
+   }
+    
+    
+    
+    
     return (
         <ul id="meals">
             { 

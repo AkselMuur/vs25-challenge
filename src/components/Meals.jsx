eb23d88fcd9b86b5ec539ec3ee79dc7a3110efb2
Kait-e <Kait-evander.uusimaa@voco.ee>
2026-04-02 13:03:56 +0300

    YL4 - meals console.2

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,30 +1,52 @@
+import { useState, useEffect } from "react";
+
 
 const Meals = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const [showError, setShowError] = useState(false);
 
-   fetchData();
-   async function fetchData() {
-    try{
-        const response = await fetch("http://localhost:3001/meals")
-        
+  useEffect(() => {
+    const getExpenses = async () => {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3001/meals");
         const data = await response.json();
-        console.log(data);
-    }
-    catch(error){
-        console.error(error);
-    }
-    
-   }
-    
-    
-    
-    
-    return (
-        <ul id="meals">
-            { 
-                // list of meals
-            }
-        </ul>
-    )
-}
 
-export default Meals
+        if (!response.ok) throw new Error("Failed fetching data");
+
+        setExpenses(data.expenses);
+        console.log(data);
+
+      } catch (error) {
+        setError({
+          title: "An error occurred!",
+          message: "Failed fetching expenses data, please try again later.",
+        });
+        setShowError(true);
+      }
+
+      setIsFetching(false);
+    };
+
+    getExpenses();
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+
+
+
+
+
+
+
+
+
+  return <ul id="meals">{}</ul>;
+};
+
+export default Meals;

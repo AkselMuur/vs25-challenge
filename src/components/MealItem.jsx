fab2b7df955687fdf1b6058d28e49fe32a77d791
Kait-e <Kait-evander.uusimaa@voco.ee>
2026-04-02 13:55:21 +0300

    YL5 valuuta

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,12 @@
 const MealItem = (props) => {
+    
+    const euroFormatter = new Intl.NumberFormat("et-EE", {
+  style: "currency",
+  currency: "EUR",
+});
+
+    
+    
   return (
     <li>
       <article className="meal-item">
@@ -9,7 +17,7 @@ const MealItem = (props) => {
         />
         <div>
           <h3 className="meal-item h3">{props.meal.name}</h3>
-          <p className="meal-item-price">{props.meal.price}</p>
+          <p className="meal-item-price">{euroFormatter.format(props.meal.price)}</p>
           <p className="meal-item-description">{props.meal.description}</p>
         </div>
         <p>
